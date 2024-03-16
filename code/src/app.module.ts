eb00f223f34af20212382a5ad8a323e1e16a741f
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-03-17 00:54:24 +0800

    尝试更改为阿里云函数计算模式

--- code/src/app.module.ts
+++ code/src/app.module.ts
@@ -1,9 +1,34 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        END_POINT: Joi.string().required(),
+        INTERNAL_END_POINT: Joi.string().required(),
+        LARK_APP_ID: Joi.string().required(),
+        LARK_APP_SECRET: Joi.string().required(),
+        MONGO_INITDB_ROOT_PASSWORD: Joi.string().required(),
+        MONGO_INITDB_ROOT_USERNAME: Joi.string().required(),
+        MONGO_SERVER_IP: Joi.string().required(),
+        OSS_ACCESS_KEY_ID: Joi.string().required(),
+        OSS_ACCESS_KEY_SECRET: Joi.string().required(),
+        OSS_BUCKET: Joi.string().required(),
+        REDIS_PASSWORD: Joi.string().required(),
+        REDIS_SERVER_IP: Joi.string().required(),
+        HTTP_SECRET: Joi.string().required(),
+        HTTP_JWT_KEY: Joi.string().required(),
+        MYSQL_ROOT_PASSWORD: Joi.string().required(),
+        MYSQL_SERVER_IP: Joi.string().required(),
+        MYSQL_DBNAME: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
