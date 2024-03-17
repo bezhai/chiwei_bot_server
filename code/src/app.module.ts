30835587da23f62beb473d588cd35e554f7175d6
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-03-17 16:33:25 +0800

    修复redis错误

--- code/src/app.module.ts
+++ code/src/app.module.ts
@@ -1,4 +1,3 @@
-import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,25 +9,6 @@ import { RedisModule } from './database/redis/redis.module';
     RedisModule,
     ConfigModule.forRoot({
       isGlobal: true,
-      validationSchema: Joi.object({
-        END_POINT: Joi.string().required(),
-        INTERNAL_END_POINT: Joi.string().required(),
-        LARK_APP_ID: Joi.string().required(),
-        LARK_APP_SECRET: Joi.string().required(),
-        MONGO_INITDB_ROOT_PASSWORD: Joi.string().required(),
-        MONGO_INITDB_ROOT_USERNAME: Joi.string().required(),
-        MONGO_HOST: Joi.string().required(),
-        OSS_ACCESS_KEY_ID: Joi.string().required(),
-        OSS_ACCESS_KEY_SECRET: Joi.string().required(),
-        OSS_BUCKET: Joi.string().required(),
-        REDIS_PASSWORD: Joi.string().required(),
-        REDIS_HOST: Joi.string().required(),
-        HTTP_SECRET: Joi.string().required(),
-        HTTP_JWT_KEY: Joi.string().required(),
-        MYSQL_ROOT_PASSWORD: Joi.string().required(),
-        MYSQL_HOST: Joi.string().required(),
-        MYSQL_DBNAME: Joi.string().required(),
-      }),
     }),
   ],
   controllers: [AppController],
--- code/src/database/redis/redis.module.ts
+++ code/src/database/redis/redis.module.ts
@@ -1,16 +1,31 @@
 import { Module } from '@nestjs/common';
-import { RedisModule as NestRedisModule } from 'nestjs-redis';
+import {
+  RedisModule as NestRedisModule,
+  RedisModuleOptions,
+} from '@nestjs-modules/ioredis';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     NestRedisModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        host: configService.get<string>('REDIS_HOST', 'localhost'),
-        password: configService.get<string>('REDIS_PASSWORD', ''),
-        port: configService.get<number>('REDIS_PORT', 6379),
-      }),
+      useFactory: (configService: ConfigService): RedisModuleOptions => {
+        const redisHost = configService.get<string>('REDIS_HOST', 'localhost');
+        const redisPort = configService.get<string>('REDIS_PORT', '6379');
+        const redisPassword = configService.get<string>('REDIS_PASSWORD');
+        const redisDb = configService.get<string>('REDIS_DB', '0');
+
+        // 构建 url 字符串
+        const url = `redis://${redisHost}:${redisPort}/${redisDb}`;
+
+        return {
+          type: 'single',
+          url,
+          options: {
+            password: redisPassword,
+          },
+        };
+      },
       inject: [ConfigService],
     }),
   ],
